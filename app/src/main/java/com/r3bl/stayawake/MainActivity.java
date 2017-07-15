1dda764598b7883553edebda0c2880552db2e4a2
Nazmul Idris <nazmul@fasterlap.com>
2017-07-15 12:45:09 -0700

    Making changes to the UI to update it to the new spec in the Sketch file.
    - Added a lot of new styles and colors.
    - Removed the toolbar, and made statusbar transparent.
    - Added a set of fonts that will be used in the /docs/fonts/ folder.
    
    Next step:
    - Evaluate the use of Android O font resources. If that doesn't work, do it the old way (using code).

--- app/src/main/java/com/r3bl/stayawake/MainActivity.java
+++ app/src/main/java/com/r3bl/stayawake/MainActivity.java
@@ -17,10 +17,12 @@
 
 package com.r3bl.stayawake;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.text.util.LinkifyCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.util.Linkify;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
@@ -33,8 +35,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //showAppIconInActionBar();
+        //hideStatusBar();
         setContentView(R.layout.activity_main);
         formatMessage();
+    }
+
+    private void hideStatusBar() {
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null ){
+            actionBar.hide();
+        }
     }
 
     private void showAppIconInActionBar() {
