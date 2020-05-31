3e42792907f966d30b2c1c43b50defce224570b2
Nazmul Idris <nazmul@fasterlap.com>
2020-05-31 12:30:59 -0700

    Update copyright, refactor
    - Remove needless code.
    - Remove needless configuration settings.
    - Stop using deprecated APIs.
    - Refactor entire code base.
    - Update copyrights.

--- app/src/main/java/com/r3bl/stayawake/HandleNotifications.java
+++ app/src/main/java/com/r3bl/stayawake/HandleNotifications.java
@@ -1,18 +1,17 @@
 /*
- * Copyright 2017 R3BL LLC.
+ * Copyright 2020 R3BL LLC. All rights reserved.
  *
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor
- * license agreements. See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership. The ASF licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed
- * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.r3bl.stayawake;
@@ -21,6 +20,7 @@ import android.annotation.TargetApi;
 import android.app.*;
 import android.content.Context;
 import android.content.Intent;
+
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
@@ -35,62 +35,43 @@ public static final int SMALL_ICON              = R.drawable.ic_stat_visibility;
 public static final int STOP_ACTION_ICON        = R.drawable.ic_stat_flare;
 
 /** PendingIntent to stop the service. */
-private static PendingIntent getStopServicePI(Service context) {
-  PendingIntent piStopService;
-  {
-    Intent
-        iStopService =
-        new MyIntentBuilder(context).setCommand(Command.STOP).build();
-    piStopService =
-        PendingIntent.getService(context, getRandomNumber(), iStopService, 0);
-  }
-  return piStopService;
+private static PendingIntent getPendingIntentToStopService(Service context) {
+  Intent intentToStopService = new MyIntentBuilder(context).setCommand(Command.STOP).build();
+  return PendingIntent.getService(context, getRandomNumber(), intentToStopService, 0);
 }
 
 /** Get pending intent to launch the activity. */
-private static PendingIntent getLaunchActivityPI(Service context) {
-  PendingIntent piLaunchMainActivity;
-  {
-    Intent iLaunchMainActivity = new Intent(context, MainActivity.class);
-    piLaunchMainActivity =
-        PendingIntent.getActivity(context,
-                                  getRandomNumber(),
-                                  iLaunchMainActivity,
-                                  0);
-  }
-  return piLaunchMainActivity;
+private static PendingIntent getPendingIntentToLaunchActivity(Service context) {
+  Intent intentToLaunchMainActivity = new Intent(context, MainActivity.class);
+  return PendingIntent.getActivity(context, getRandomNumber(), intentToLaunchMainActivity, 0);
 }
 
 // Pre O specific.
 
 @TargetApi(25)
 public static class PreO {
-
   public static void createNotification(Service context) {
-    // Create Pending Intents.
-    PendingIntent piLaunchMainActivity = getLaunchActivityPI(context);
-    PendingIntent piStopService = getStopServicePI(context);
-
     // Action to stop the service.
     NotificationCompat.Action stopAction =
         new NotificationCompat.Action.Builder(
             STOP_ACTION_ICON,
             getNotificationStopActionText(context),
-            piStopService)
+            getPendingIntentToStopService(context))
             .build();
 
     // Create a notification.
-    Notification mNotification =
-        new NotificationCompat.Builder(context)
+    // More information on ignored Channel ID: https://stackoverflow.com/a/45580202/2085356
+    Notification notification =
+        new NotificationCompat.Builder(context, "")
             .setContentTitle(getNotificationTitle(context))
             .setContentText(getNotificationContent(context))
             .setSmallIcon(SMALL_ICON)
-            .setContentIntent(piLaunchMainActivity)
+            .setContentIntent(getPendingIntentToLaunchActivity(context))
             .addAction(stopAction)
             .setStyle(new NotificationCompat.BigTextStyle())
             .build();
 
-    context.startForeground(ONGOING_NOTIFICATION_ID, mNotification);
+    context.startForeground(ONGOING_NOTIFICATION_ID, notification);
   }
 }
 
@@ -104,13 +85,10 @@ private static String getNotificationTitle(Service context) {
   return context.getString(R.string.notification_text_title);
 }
 
-//
 // O Specific.
-//
 
 @TargetApi(26)
 public static class O {
-
   public static final String CHANNEL_ID = String.valueOf(getRandomNumber());
 
   public static void createNotification(Service context) {
@@ -121,26 +99,22 @@ public static class O {
 
   private static Notification buildNotification(Service context,
                                                 String channelId) {
-    // Create Pending Intents.
-    PendingIntent piLaunchMainActivity = getLaunchActivityPI(context);
-    PendingIntent piStopService = getStopServicePI(context);
-
     // Action to stop the service.
-    Notification.Action stopAction =
-        new Notification.Action.Builder(
+    NotificationCompat.Action stopAction =
+        new NotificationCompat.Action.Builder(
             STOP_ACTION_ICON,
             getNotificationStopActionText(context),
-            piStopService)
+            getPendingIntentToStopService(context))
             .build();
 
     // Create a notification.
-    return new Notification.Builder(context, channelId)
+    return new NotificationCompat.Builder(context, channelId)
                .setContentTitle(getNotificationTitle(context))
                .setContentText(getNotificationContent(context))
                .setSmallIcon(SMALL_ICON)
-               .setContentIntent(piLaunchMainActivity)
-               .setActions(stopAction)
-               .setStyle(new Notification.BigTextStyle())
+               .setContentIntent(getPendingIntentToLaunchActivity(context))
+               .addAction(stopAction)
+               .setStyle(new NotificationCompat.BigTextStyle())
                .build();
   }
 
