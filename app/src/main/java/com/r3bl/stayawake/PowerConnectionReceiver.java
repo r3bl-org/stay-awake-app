8cab0add1972cf75fc107d02ea9a8aeef033cc3d
Nazmul Idris <nazmul@fasterlap.com>
2020-05-31 13:41:05 -0700

    For debugging, add toast to show power changes

--- app/src/main/java/com/r3bl/stayawake/PowerConnectionReceiver.java
+++ app/src/main/java/com/r3bl/stayawake/PowerConnectionReceiver.java
@@ -19,37 +19,51 @@ package com.r3bl.stayawake;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.widget.Toast;
 
 import static android.util.Log.d;
 import static com.r3bl.stayawake.MyTileService.TAG;
 
 /** Changes to Android broadcast receiver behaviors: http://tinyurl.com/y9rm5wzg */
 public class PowerConnectionReceiver extends BroadcastReceiver {
+
+private Context context;
+
 public void onReceive(Context context, Intent intent) {
+  this.context = context;
   String action = intent.getAction();
-  d(TAG, "onReceive: Action=" + action);
+  String msg1 = "onReceive: Action=" + action;
+  d(TAG, msg1);
   switch (action) {
     case Intent.ACTION_POWER_CONNECTED:
-      powerConnected(context);
+      powerConnected();
       break;
     case Intent.ACTION_POWER_DISCONNECTED:
       powerDisconnected();
+      break;
+    default:
+      showToast(msg1);
       break;
   }
 }
 
 // Do nothing when power disconnected.
 private void powerDisconnected() {
-  d(TAG, "PowerConnectionReceiver onReceive(): ACTION_POWER_DISCONNECTED");
-  d(TAG, "Do nothing");
+  String msg1 = "PowerConnectionReceiver onReceive(): ACTION_POWER_DISCONNECTED ... Do nothing";
+  showToast(msg1);
+  d(TAG, msg1);
 }
 
 // Start service when power connected.
-private void powerConnected(Context context) {
-  d(TAG, "PowerConnectionReceiver onReceive(): ACTION_POWER_CONNECTED");
-  d(TAG, "Start Service");
+private void powerConnected() {
   Intent startServiceIntent = new MyIntentBuilder(context).setCommand(Command.START).build();
   context.startService(startServiceIntent);
+  String msg1 = "PowerConnectionReceiver onReceive(): ACTION_POWER_CONNECTED ... Start Service";
+  showToast(msg1);
+  d(TAG, msg1);
+}
+
+private void showToast(String msg1) {
+  Toast.makeText(context, msg1, Toast.LENGTH_LONG).show();
 }
 }
